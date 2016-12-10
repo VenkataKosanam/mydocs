1738b91f610086d8c94d7b58b2622fa2c76b6b29
VenkataKosanam <s9922169799@gmail.com>
2016-12-09 20:33:35 -0500

    sample one

---
+++ SampleWork/src/test/FirstSample.java
@@ -0,0 +1,13 @@
+package test;
+
+public class FirstSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+		System.out.println("Welcome");
+		System.out.println("Welcome");
+		//Venkata Kosnam
+	}
+
+}
